a49755ae7e81f29d39153f64ec3a8b182b440365
saca12 <salomoncompaore22@gmail.com>
2026-02-23 12:51:56 +0000

    Correction du build

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 set -o errexit
 
+# Installation propre
 pip install -r requirements.txt
+
+# Préparation des fichiers pour la prod
 python manage.py collectstatic --no-input
-python manage.py migrate
+
+# Migration vers la base de données configurée dans DATABASE_URL
+python manage.py migrate --no-input
