66bbfa8b8bf746f087cd03dff24ea9e5031a8758
saca12 <salomoncompaore22@gmail.com>
2026-02-23 11:44:42 +0000

    db setting 2

--- setting.py
+++ setting.py
@@ -6,14 +6,21 @@ DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['*'] # À affiner plus tard avec ton URL Render
 
 # Base de données (Utilise la variable d'env DATABASE_URL de Render)
-DATABASES = {
-    'default': dj_database_url.config(
-        # Cette ligne récupère la variable DATABASE_URL de Render
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
-}
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB", "tuteur_ia"),
+            "USER": os.getenv("POSTGRES_USER", "tuteur_ia"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD", "tuteur_ia"),
+            "HOST": os.getenv("POSTGRES_HOST", "localhost"),
+            "PORT": os.getenv("POSTGRES_PORT", "5432"),
+        }
+    }
 # Fichiers statiques (indispensable pour WhiteNoise)
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
